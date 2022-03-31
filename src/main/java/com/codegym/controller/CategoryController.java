cfaa24a87afed1a056738d8cc2ca590b6b265720
TriHoangZXC <hoanghuutri97@gmail.com>
2022-03-31 16:56:11 +0700

    demo CRUD 2 bang su dung RESTful

---
+++ src/main/java/com/codegym/controller/CategoryController.java
@@ -0,0 +1,72 @@
+package com.codegym.controller;
+
+import com.codegym.model.Category;
+import com.codegym.model.Product;
+import com.codegym.service.category.ICategoryService;
+import com.codegym.service.product.IProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/categories")
+public class CategoryController {
+    @Autowired
+    private ICategoryService categoryService;
+
+    @Autowired
+    private IProductService productService;
+
+    @GetMapping
+    public ResponseEntity<Iterable<Category>> findAllCategory(){
+        Iterable<Category> categories = categoryService.findAll();
+        return new ResponseEntity<>(categories, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Category> saveCategory(@RequestBody Category category) {
+        return new ResponseEntity<>(categoryService.save(category), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Category> findCategoryById(@PathVariable Long id){
+        Optional<Category> categoryOptional = categoryService.findById(id);
+        if (!categoryOptional.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(categoryOptional.get(), HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Category> updateCategory(@PathVariable Long id, @RequestBody Category newCategory){
+        Optional<Category> categoryOptional = categoryService.findById(id);
+        if (!categoryOptional.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        newCategory.setId(id);
+        return new ResponseEntity<>(categoryService.save(newCategory), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}/products")
+    public ResponseEntity<Iterable<Product>> findAllByCategory(@PathVariable Long id){
+        Optional<Category> categoryOptional = categoryService.findById(id);
+        if (!categoryOptional.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        Iterable<Product> products = productService.findAllByCategory(categoryOptional.get());
+        return new ResponseEntity<>(products, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Category> deleteAllProductByCategory(@PathVariable Long id) {
+        Optional<Category> categoryOptional = categoryService.findById(id);
+        if (!categoryOptional.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        categoryService.deleteAllProductByCategory(id);
+        return new ResponseEntity<>(categoryOptional.get(), HttpStatus.OK);
+    }
+}
---
+++ src/main/java/com/codegym/controller/ProductController.java
@@ -0,0 +1,70 @@
+package com.codegym.controller;
+
+import com.codegym.model.Category;
+import com.codegym.model.Product;
+import com.codegym.service.category.ICategoryService;
+import com.codegym.service.product.IProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+    @Autowired
+    private IProductService productService;
+
+    @Autowired
+    private ICategoryService categoryService;
+
+    @ModelAttribute("categories")
+    public Iterable<Category> categories() {
+        return categoryService.findAll();
+    }
+
+    @GetMapping
+    public ResponseEntity<Iterable<Product>> findAllProduct(@RequestParam (name = "q") Optional<String> q){
+        Iterable<Product> products = productService.findAll();
+        if (q.isPresent()) {
+            products = productService.findProductByNameContaining(q.get());
+        }
+        return new ResponseEntity<>(products, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Product> saveProduct(@RequestBody Product product){
+        return new ResponseEntity<>(productService.save(product), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> findProductById(@PathVariable Long id){
+        Optional<Product> productOptional = productService.findById(id);
+        if (!productOptional.isPresent()){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(productOptional.get(), HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Product> updateProduct(@PathVariable Long id, @RequestBody Product newProduct){
+        Optional<Product> productOptional = productService.findById(id);
+        if (!productOptional.isPresent()){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        newProduct.setId(id);
+        return new ResponseEntity<>(productService.save(newProduct), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Product> deleteProduct(@PathVariable Long id) {
+        Optional<Product> productOptional = productService.findById(id);
+        if (!productOptional.isPresent()){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        productService.remove(id);
+        return new ResponseEntity<>(productOptional.get(), HttpStatus.OK);
+    }
+}
